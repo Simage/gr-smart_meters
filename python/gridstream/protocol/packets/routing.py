963b510c4143157831ab28b954cf599f8edc0a45
Simage <simage@gmail.com>
2021-06-25 00:48:52 -0600

    move source and destination to base packet

--- python/gridstream/protocol/packets/routing.py
+++ python/gridstream/protocol/packets/routing.py
@@ -4,14 +4,13 @@ from .packet import Packet
 
 class RoutingPacket(Packet):
     frame_type = b"\x55"
+    destination = BytesField(1, 6)
+    source = BytesField(7, 6)
 
 
 @RoutingPacket.register(0x30)
 class ARPPacket(RoutingPacket):
-    destination = BytesField(1, 6)
-    source = BytesField(7, 6)
     sequence = ByteField(13)
     uptime = IntegerField(14, 4)
     lan_address = BytesField(20, 4)
     timing = IntegerField(29, 2)
-    pass
